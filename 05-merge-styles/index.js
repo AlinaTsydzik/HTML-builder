56cfea7397fe853767da63cd4cf7785965fe961e
Alina Tsydzik <pronevich.alina@gmail.com>
2023-05-08 14:37:34 +0200

    feat: add 5th solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, files) => {
+  if (err) throw err;
+
+  fs.truncate(path.join(__dirname, 'project-dist', 'bundle.css'), err => {
+    if(err) throw err;
+ });
+
+  files.forEach(file => {
+      if(file.isFile()){
+        const i = file.name.lastIndexOf('.');
+        const fileExt = file.name.slice(i+1);
+        if(fileExt === 'css'){
+          const input = fs.createReadStream(path.join(__dirname, 'styles', file.name));
+          input.on('data', data => {
+            fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data.toString(), (err) => {
+              if (err) throw err;
+            })
+          })
+        }
+      }
+  });
+})
+
+
